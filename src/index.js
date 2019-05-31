52e5a1a052e18c217e60d235e79a555e7447446a
Albert Lyubarsky <albertly@yandex.ru>
2019-05-31 21:45:47 +0300

    Added react-bootstrap, no more JQuery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'jquery/dist/jquery.min.js';
-import  'bootstrap/dist/js/bootstrap.min.js';
 
 import './index.css';
 
--- src/shared/simple-modal.js
+++ src/shared/simple-modal.js
@@ -1,27 +1,21 @@
 import React from 'react';
+import { Modal } from 'react-bootstrap';
 
 function SimpleModal(props) {
     
-    let modalVisibility = "modal fade";
-    let modalStyle = {display: 'none', opacity:0};
-    if (props.show) {
-        modalVisibility = "modal"
-        modalStyle = {display: 'block', opacity:1};
-    }
     return (
-        <div id={props.elementId} style={modalStyle} className={modalVisibility} tabIndex="-1" role="dialog">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="close" onClick={props.onClose} data-dismiss="modal"><span>&times;</span></button>
-                        <h4 className="modal-title">{props.title}</h4>
-                    </div>
-                    <div className="modal-body" style={{height: "250px", overflowY: "scroll"}}>
+        <Modal id={props.elementId} show={props.show} onHide={props.onClose}>
+           
+                {/* <div className="modal-content"> */}
+                    <Modal.Header closeButton>
+                        <Modal.Title>{props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body style={{height: "250px", overflowY: "scroll"}}>
                         {props.children}
-                    </div>
-                </div>
-            </div>
-       </div>        
+                    </Modal.Body>
+                {/* </div> */}
+        
+       </Modal>        
     );
 }
 

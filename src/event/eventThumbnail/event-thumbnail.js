a91dff4fbd53f4762072b9f2f8847fcf299f12e3
albertly <albertly@yandex.ru>
2019-09-07 00:09:12 +0300

    Payment

--- src/event/eventThumbnail/event-thumbnail.js
+++ src/event/eventThumbnail/event-thumbnail.js
@@ -1,40 +1,41 @@
 import React from 'react'
-import { Panel, Button, Glyphicon, ButtonToolbar, ButtonGroup  } from 'react-bootstrap';
+import { Panel, Button, Glyphicon, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
+import StripeCheckout from 'react-stripe-checkout';
 
 import styles from './event-thumbnail.module.css';
 
 const EventThumbnail = props => {
-    let place =  '';
-    let startTime = '';
+  let place = '';
+  let startTime = '';
 
-    const getStartTimeStyle = () => {
-      if (props.event.time === '8:00 am')
-        return {color: '#003300', 'fontWeight': 'bold'}
-      return {}
-    }
+  const getStartTimeStyle = () => {
+    if (props.event.time === '8:00 am')
+      return { color: '#003300', 'fontWeight': 'bold' }
+    return {}
+  }
 
-    switch(props.event.time) {
-      case '8:00 am': startTime = ' (Early Start)'; break;
-      case '10:00 am': startTime = ' (Late Start)'; break;
-      default : startTime = ' (Normal Start)';
-    }
+  switch (props.event.time) {
+    case '8:00 am': startTime = ' (Early Start)'; break;
+    case '10:00 am': startTime = ' (Late Start)'; break;
+    default: startTime = ' (Normal Start)';
+  }
 
-    if (!props.event.location) {
-      place =  <div>
-                 Online URL: {props.event.onlineUrl}
-                </div>;
-    }
-    else {
-      place =  <div>
-                  <span>Location: {props.event.location.address}</span>
-                  <span className={styles['pad-left']}>{props.event.location.city}, {props.event.location.country}</span>
-                </div>;   
-    }
+  if (!props.event.location) {
+    place = <div>
+      Online URL: {props.event.onlineUrl}
+    </div>;
+  }
+  else {
+    place = <div>
+      <span>Location: {props.event.location.address}</span>
+      <span className={styles['pad-left']}>{props.event.location.city}, {props.event.location.country}</span>
+    </div>;
+  }
 
-    return (
-      <Panel  onClick={() => props.onClickHandler(props.event._id)} className={[ styles.well, styles.thumbnail].join(' ')}>
-         <Panel.Title className={styles.fr}>
-         <ButtonToolbar >
+  return (
+    <Panel onClick={() => props.onClickHandler(props.event._id)} className={[styles.well, styles.thumbnail].join(' ')}>
+      <Panel.Title className={styles.fr}>
+        <ButtonToolbar >
           <ButtonGroup >
             <Button bsSize="xsmall" onClick={(e) => props.onDeleteEventHandler(e, props.event._id)}>
               <Glyphicon glyph="trash" />
@@ -42,16 +43,30 @@ const EventThumbnail = props => {
             <Button bsSize="xsmall">
               <Glyphicon glyph="pencil" />
             </Button>
-            <Button bsSize="xsmall">
-              <Glyphicon glyph="shopping-cart" />
-            </Button>
+            <span onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+              }}>
+            <StripeCheckout
+              name="Emaily"
+              description="$5 for 5 email credits"
+              amount={500}
+              token={token => console.log('token',token)}
+              stripeKey={'pk_test_j3acsib29tsMFPqakQN0pc8T00gvFoiYIH'}
+            >
+              <Button bsSize="xsmall" >
+                <Glyphicon glyph="shopping-cart" />
+              </Button>
+
+            </StripeCheckout>
+            </span>
             <Button bsSize="xsmall">
               <Glyphicon glyph="list" />
             </Button>
           </ButtonGroup>
-          </ButtonToolbar>
-         </Panel.Title>
-         <Panel.Body>
+        </ButtonToolbar>
+      </Panel.Title>
+      <Panel.Body>
         <h2>{props.event.name.toUpperCase()}</h2>
         <div>Date: {props.event.date}</div>
         <div style={getStartTimeStyle()}>Time: {props.event.time}
@@ -59,9 +74,9 @@ const EventThumbnail = props => {
         </div>
         <div>Price: ${props.event.price}</div>
         {place}
-        </Panel.Body>
-      </Panel>
-    );
-  }
-  
-  export default EventThumbnail;
+      </Panel.Body>
+    </Panel>
+  );
+}
+
+export default EventThumbnail;

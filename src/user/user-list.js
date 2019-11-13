8d1b585bf187ebdf186bd392b6d95379c02213e8
Albert Lyubarsky <albert.l@comtecglobal.com>
2019-11-13 17:35:59 +0200

    Adding delete button on grid row. Needs fix

--- src/user/user-list.js
+++ src/user/user-list.js
@@ -6,12 +6,30 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
 
+function FileCellRenderer({onRemoveSelected}) {
+//
+        console.log(onRemoveSelected);
+        return (
+            <div>
+                    <button onClick={onRemoveSelected}>x1</button>
+            </div>
+        );
+
+
+}
+
 
 export function UserList({ history }) {
     const [userData, setUserData] = useState([]);
     const [gridApi, setGridApi] = useState('a');
+
+    const frameworkComponents = {
+        fileCellRenderer: FileCellRenderer
+    };
+    
     const state = {
-        columnDefs: [{
+        columnDefs: [
+        {
             headerName: "Id", field: "_id", sortable: false, width: 190
         }, {
             headerName: "User Name", field: "userName", sortable: true, width: 190
@@ -23,14 +41,38 @@ export function UserList({ history }) {
             headerName: "Last Name", field: "lastName", sortable: true, width: 150
         }, {
             headerName: "Roles", field: "roles", sortable: true, width: 90
-        }, {
+         },
+
+         {
             headerName: "", field: "googleProvider", sortable: true, width: 50,
-            cellRenderer: function(params) {
-                return params.data.googleProvider ? '<img src="/assets/images/g1.png" height="20" width="40" alt=""/>' : "";
-            } 
-        }
+         },
+         {
+            headerName: "Action", width:50, 
+            cellRendererFramework: () => <button onClick={onRemoveSelected}>a2</button>
+         }
+            // function(params) {
+            //     return params.data.googleProvider ? '<img src="/assets/images/g1.png" height="20" width="40" alt=""/>' : "";
+            // }
+
+            // cellRenderer: function(params) {
+
+            //     var eDiv = document.createElement('button');
+            //     eDiv.innerHTML = '<span class="my-css-class"><button class="btn-simple">Push Me</button></span>';
+            //     var eButton = eDiv.querySelectorAll('.btn-simple')[0];
+            
+            //     eButton.addEventListener('click', function() {
+            //         onRemoveSelected();
+            //     });
+            
+            //     return eDiv;
+
+            //     //return '<button onClick={onRemoveSelected}>x</button>';
+            // }
+        
      ]        
     }
+
+
 
     useEffect(() => {
         axios.get('/api/users?limit=50')
@@ -49,15 +91,16 @@ export function UserList({ history }) {
     const onGridReady = params => {
         console.log("params ", params.api);
         setGridApi(params.api);
-        console.log("Grid API ", gridApi);
+        console.log("Grid API 1", gridApi);
+        console.log("Grid API 2", params.api);
         //gridColumnApi = params.columnApi;
         
     };
     
     const onRemoveSelected = () => {
-        console.log(gridApi);
+        console.log('Grid 1',gridApi);
         var selectedData = gridApi.getSelectedRows();
-        console.log(selectedData);
+        console.log('Data', selectedData);
        // var res = this.gridApi.updateRowData({ remove: selectedData });
        // printResult(res);
     }

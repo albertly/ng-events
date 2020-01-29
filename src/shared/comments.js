84b5e4a93733fe3a5632b1aa1504367406b24d2f
albertly <albertly@yandex.ru>
2020-01-29 16:13:27 +0200

    Get TALK url from env. var

--- src/shared/comments.js
+++ src/shared/comments.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 
-
-
 class Comments extends Component {
-
 
     componentDidMount() {
         window.Coral.createStreamEmbed({
             id: 'id001',
             autoRender: true,
-            rootURL: 'http://localhost:3000',
+            rootURL: process.env.REACT_APP_TALK_URL,
             storyID: this.props.storyID,
             storyURL: this.props.storyURL
         });
-
     }
 
     render() {
